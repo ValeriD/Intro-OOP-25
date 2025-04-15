bead3f5a3dce4199311da8e2a12ad9852bdfd952
Valeri <valer.dobrev10@gmail.com>
2025-04-15 17:05:37 +0300

    Exam prep

---
+++ 10g/lesson7/exam_prep.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Device
+{
+    // The device should have memory in GB, screen size, price, brand and constant serial number.
+    // There should be a counter for the number of active devices(instances)
+
+public:
+    Device(double memorySize, double screenSize, double price, const char *brand, const char *constantSerial);
+
+    Device(const Device &other);
+    Device &operator=(const Device &other);
+
+    /*
+         (memory*size) / price greater than 0.1 is worth buying
+    */
+    bool isWorthBuying() const;
+
+    // Getters and setters
+};
+
+class Phone : public Device
+{
+    // The phone should have camera parameters field
+
+public:
+    Phone(double memorySize, double screenSize, double price, const char *brand, const char *constantSerial, int cameraParameters);
+    Phone(const Phone &other);
+    Phone &operator=(const Phone &other);
+
+    /*
+         (memory*size) + (memory*cameraParameters) / price greater than 0.1 is worth buying
+    */
+    bool isWorthBuying() const;
+
+    // Getters and setters
+};
